e72fb5f53e69f34e8c6da83f8a98c36ab14d1afd
Lucas Albuquerque <lucas.albuquerque.gk@gmail.com>
2021-11-23 21:05:29 -0300

    API com NestJS # 2. Configurando um banco de dados PostgreSQL com TypeORM

--- src/post/entities/post.entity.ts
+++ src/post/entities/post.entity.ts
@@ -1,3 +1,13 @@
-export class Post {
-    id: number
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm';
+ 
+@Entity()
+export class Post extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  public id: number;
+ 
+  @Column()
+  public title: string;
+ 
+  @Column()
+  public content: string;
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -2,47 +2,47 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { Post } from './entities/post.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PostService {
-  private lastPostId = 0;
-  private posts: Post[] = [];
 
-  create(post: CreatePostDto) {
-    const newPost = {
-      id: ++this.lastPostId,
-      ...post
-    }
-    this.posts.push(newPost);
+  constructor(
+    @InjectRepository(Post)
+    private postsRepository: Repository<Post>
+  ) { }
+
+  async create(post: CreatePostDto) {
+    const newPost = await this.postsRepository.create(post);
+    await this.postsRepository.save(newPost);
     return newPost;
   }
 
   findAll() {
-    return this.posts;
+    return this.postsRepository.find();
   }
 
-  findOne(id: number) {
-    const post = this.posts.find(post => post.id === id);
+  async findOne(id: number) {
+    const post = await this.postsRepository.findOne(id);
     if (post) {
       return post;
     }
     throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
   }
 
-  update(id: number, post: UpdatePostDto) {
-    const postIndex = this.posts.findIndex(post => post.id === id);
-    if (postIndex > -1) {
-      this.posts[postIndex] = post;
-      return post;
+  async update(id: number, post: UpdatePostDto) {
+    await this.postsRepository.update(id, post);
+    const updatedPost = await this.postsRepository.findOne(id);
+    if (updatedPost) {
+      return updatedPost
     }
     throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
   }
 
-  remove(id: number) {
-    const postIndex = this.posts.findIndex(post => post.id === id);
-    if (postIndex > -1) {
-      this.posts.splice(postIndex, 1);
-    } else {
+  async remove(id: number) {
+    const deleteResponse = await this.postsRepository.delete(id);
+    if (!deleteResponse.affected) {
       throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
     }
   }
